763be1ab959511c3b19645d0d573ac7682fad687
Unknown <kassab_design@hotmail.com>
2018-05-22 19:04:39 +0200

    adding relation between article and tag

--- app/Article.php
+++ app/Article.php
@@ -10,4 +10,9 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
