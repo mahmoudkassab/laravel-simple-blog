5302e048b206e4f7320c3241d8c94a17f0c829f6
Unknown <kassab_design@hotmail.com>
2018-05-22 21:22:13 +0200

    adding show and showAll methods to TagController

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class TagsController extends Controller
 {
-    //
+    
+    public function show(Tag $tag)
+    {
+        $articles = $tag->articles()->where('published_at', '<', Carbon::now())->orderBy('created_at', 'desc')
+            ->get();
+        return view('articles.index', compact('articles'));
+    }
+
+    public function showAllTags(){
+        $tags =  Tag::select('id', 'name')->get();
+        return view('articles.tags', compact('tags'));
+    }
 }
