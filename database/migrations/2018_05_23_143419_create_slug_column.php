b774fdc70270bbaf84031ddcd01b409e49504a8e
Unknown <kassab_design@hotmail.com>
2018-05-23 16:36:27 +0200

    editing slug migration with text column

--- database/migrations/2018_05_23_143419_create_slug_column.php
+++ database/migrations/2018_05_23_143419_create_slug_column.php
@@ -14,7 +14,7 @@ class CreateSlugColumn extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->text('slug');
         });
     }
 
