4e531b4f92bc3a1dd8ea43a91bd5c71572dde16b
Unknown <kassab_design@hotmail.com>
2018-05-23 16:29:00 +0200

    adding comment area in show blade

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -19,5 +19,39 @@
         @endforeach
     @endunless
     <hr>
+    <p>Comments:
+    <table class="table table-hover">
+        <tbody>
+        @foreach($article->comments as $comment)
+            <tr>
+                <td>{{ $comment->user->name }}:</td>
+                <td>
+                    {{ $comment->comment }}
+                </td>
+                <td>
+                    @if($comment->user_id == Auth::id())
+                        <form method="post" action="{{ route('commentDelete',$comment->id) }}">
+                            {{ csrf_field() }}
+                            <input name="_method" type="hidden" value="DELETE">
+                            <input type="submit" value="Delete" class="btn btn-danger">
+                        </form>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+    <form method="post" action="{{ route('commentStore',$article->id) }}">
+        {{ csrf_field() }}
+        <input name="article_id" type="hidden" value="{{ $article->id }}">
+        <input name="user_id" type="hidden" value="{{ Auth::id() }}">
+        <div class="form-group row">
+            <label for="body" class="col-sm-2 col-form-label">Add Comment: </label>
+
+            <textarea name="comment" id="comment" cols="1" rows="1" class="form-control"></textarea>
+        </div>
+        <input type="submit" value="Add comment" class="btn btn-success">
+    </form>
 
 @stop
